92e5c2f68015bc0b02e64c06d654160412c81e32
Alexander <alex.ermakov631@gmail.com>
2020-11-01 00:42:13 +0300

    init

---
+++ src/main/java/com/sweeter/domain/Message.java
@@ -0,0 +1,74 @@
+package com.sweeter.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String text_message;
+
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String filename;
+
+    public Message() {
+    }
+
+    public Message(String text_message, String tag, User user) {
+        this.author = user;
+        this.text_message = text_message;
+        this.tag = tag;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText_message() {
+        return text_message;
+    }
+
+    public void setText_message(String text_message) {
+        this.text_message = text_message;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
